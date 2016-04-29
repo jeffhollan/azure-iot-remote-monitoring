3596df397f498524bc0f004548ad0f7d238115cd
Jeff Hollan <jeffhollan@outlook.com>
2016-04-29 10:33:12 -0700

    Update ActionRepository.cs

--- DeviceAdministration/Infrastructure/Repository/ActionRepository.cs
+++ DeviceAdministration/Infrastructure/Repository/ActionRepository.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.Repository
@@ -10,23 +12,38 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infr
     public class ActionRepository : IActionRepository
     {
         // Currently this list is not editable in the app
-        private List<string> _actionIds = new List<string>()
+        private Dictionary<string,string> _actionIds = new Dictionary<string, string>()
         {
-            "Send Message",
-            "Raise Alarm"
+            { "Send Message", "https://prod-07.eastus.logic.azure.com:443/workflows/037b0fdb254d4b359de46dda62ca8503/triggers/manual/run?api-version=2015-08-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=hqLBsRShN0Uf8UwRW9zOf-MkPpj-yaz5E8aL90XrMUI" },
+            { "Raise Alarm", "https://prod-07.eastus.logic.azure.com:443/workflows/037b0fdb254d4b359de46dda62ca8503/triggers/manual/run?api-version=2015-08-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=hqLBsRShN0Uf8UwRW9zOf-MkPpj-yaz5E8aL90XrMUI" }
         };
 
         public async Task<List<string>> GetAllActionIdsAsync()
         {
-            return await Task.Run(() => { return _actionIds; });
+            return await Task.Run(() => { return new List<string>(_actionIds.Keys); });
         }
 
         public async Task<bool> ExecuteLogicAppAsync(string actionId, string deviceId, string measurementName, double measuredValue)
         {
-            Debug.WriteLine("ExecuteLogicAppAsync is not yet implemented");
+            return await Task.Run(async () =>
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.PostAsync(_actionIds[actionId],
+                        new StringContent(JsonConvert.SerializeObject(
+                            new
+                            {
+                                deviceId = deviceId,
+                                measurementName = measurementName,
+                                measuredValue = measuredValue
+                            }),
+                            System.Text.Encoding.UTF8,
+                            "application/json"));
 
-            await Task.Run(() => { });
-            return false;
+                    return response.IsSuccessStatusCode;
+                }
+            });
+            
         }
     }
 }
